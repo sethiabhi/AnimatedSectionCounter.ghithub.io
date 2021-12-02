93053903f2bfc016854c9413a29dbeaa33d23500
sethiabhi <36853897+sethiabhi@users.noreply.github.com>
2021-12-02 16:00:46 +0530

    Animated Section Counter.
    Animated section counter is developed using HTML CSS and JavaScript.

---
+++ myScript.js
@@ -0,0 +1,18 @@
+const counters = document.querySelectorAll('.counter');
+counters.forEach((counter)=>{
+    // console.log(counter);
+    counter.innerHTML = 0;
+    const updateCounter = () =>{
+        const targetCount = +counter.getAttribute('data-target');
+        // console.log(targetCount);
+        const startingCount = Number(counter.innerHTML);
+        const incr = targetCount / 100;
+        if(startingCount < targetCount){
+            counter.innerHTML = `${Math.round(startingCount + incr)}`;
+            setTimeout(updateCounter,10);
+        }else{
+            counter.innerHTML = targetCount;
+        }
+    }
+    updateCounter();
+})
